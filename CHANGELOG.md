81be06285096085de5c7b5be6b15e9b863710058
Kim, Jong-kook <kjk15881588@gmail.com>
2020-12-03 18:59:43 +0900

    번역값 추가

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased - 
 
+## 1.5.4 - 2020-12-03
+### Added
+- 번역값 추가
+
 ## 1.5.3 - 2020-12-02
 ### Added
 - 번역 일부 추가
--- test/flutter_jk_test.dart
+++ test/flutter_jk_test.dart
@@ -3,5 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_jk/flutter_jk.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('GetXMessages test', () {
+    final msgs = GetxMessages();
+    final len = msgs.keys.entries.first.value.length;
+    for (var key in msgs.keys.keys) {
+      expect(msgs.keys[key].length, len, reason: '언어별로 모든 메시지의 개수는 동일해야함.');
+    }
+  });
 }
